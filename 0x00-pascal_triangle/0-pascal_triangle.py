873d850b3a4da2ace80e74c4a577c134bc3eb75c
maqamylee0 <maqamylee0@gmail.com>
2024-04-28 23:25:38 +0000

    fix stle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
     scripts calculates pascal triangle using recursion
 """
 
+
 def generate_row(row_number, prev_row):
     """method generates row in the triangle"""
     if row_number == 0:
@@ -15,6 +16,8 @@ def generate_row(row_number, prev_row):
             row.append(prev_row[i - 1] + prev_row[i])
         row.append(1)  # Last element of each row is always 1
         return row
+
+
 def pascal_triangle(n, current_row=0, triangle=None):
     """method calculates pasacl triangle to n levels"""
     if n <= 0:
@@ -28,5 +31,5 @@ def pascal_triangle(n, current_row=0, triangle=None):
 
     row = generate_row(current_row, triangle[-1] if triangle else [])
     triangle.append(row)
-    
+
     return pascal_triangle(n, current_row + 1, triangle)
